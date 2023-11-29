289ff76a2d653b7fa0e6fd31dc7e2f6c9d43ae37
Craig Puxty <craig_puxty@talktalk.net>
2023-11-29 20:30:03 +0000

    Updated Form with state and controlled elements.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,37 @@
+import { useState } from 'react';
+
 export default function Form() {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
-  };
 
+    if (!description) return;
+
+    const newItem = { quantity, description, packed: false, id: Date.now() };
+    console.log(newItem);
+    setQuantity(1);
+    setDescription('');
+  };
   return (
     <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type='text' placeholder='Item...' />
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
