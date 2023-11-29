5e90e1adaa0882d71d85931f0eccf49a42b005cc
Craig Puxty <craig_puxty@talktalk.net>
2023-11-29 19:25:24 +0000

    Added PackingListItem component, updated PackingList.

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,3 +1,19 @@
+import PackingListItem from './PackingListItem';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
 export default function PackingList() {
-  return <div className='list'>List</div>;
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <PackingListItem item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
