49051d02d1b512e767ea6c1ad2f054e61fc27b02
Craig Puxty <craig_puxty@talktalk.net>
2023-11-29 18:43:52 +0000

    Added Header, Form, PackingList & Footer components.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import Header from './components/Header';
+import Form from './components/Form';
+import PackingList from './components/PackingList';
+import Footer from './components/Footer';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className='app'>
+      <Header />
+      <Form />
+      <PackingList />
+      <Footer />
+    </div>
+  );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='stats'>
+      <em>You have X items on your list, and you have already packed X (X%)</em>
+    </footer>
+  );
+}
