628b0d41390e83146d14722098debc38c9472953
Craig Puxty <craig_puxty@talktalk.net>
2023-11-29 21:49:44 +0000

    Added toggle item packed functionaity.

--- src/components/PackingListItem.jsx
+++ src/components/PackingListItem.jsx
@@ -1,9 +1,21 @@
 export default function PackingListItem({ item, setItems }) {
+  const handleToggleItem = (id) => {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
   const handleDeleteItem = (id) => {
     setItems((items) => items.filter((item) => item.id !== id));
   };
   return (
     <li>
+      <input
+        type='checkbox'
+        value={item.packed}
+        onChange={() => handleToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
